05b9e936cfed3293b0820058f38bf365017672d2
stevenmstx <steven@purewavedesigns.com>
2014-01-23 20:47:54 -0600

    Added comments to the variables

--- Assignments/Conditionals/Conditionals_Personal/js/scripts.js
+++ Assignments/Conditionals/Conditionals_Personal/js/scripts.js
@@ -1,12 +1,12 @@
 // Steven Spencer Jan. 23, 2014 - Conditionals
 //How much rice and water is needed to make steamed rice for a certain amount of people. The steamer is limited to a maximum serving of 20 people.
-var people = prompt("How many people will be dinning this evening?");
-var eating = prompt("How many of them will be eating rice?");
-var cook = 0;
-var rice = 2;
-var maximum = 20;
+var people = prompt("How many people will be dinning this evening?");//This is how many people are eating total
+var eating = prompt("How many of them will be eating rice?");//This is how many people are eating rice
+var cook = 0;//This is a variable to determine true or false later.
+var rice = 2;//This is the minimum number of cups needed.
+var maximum = 20;//This is the maximum number of people the rice maker can make rice for.
 
-if (people == "" && eating == "") {
+if (people == "" && eating == "") { 
     console.log("A value is required!");
 }else if(eating <= maximum){
     (eating > people) ? console.log("Imaginary friends do not count. Must be less than People dinning.") : cook = 1;
