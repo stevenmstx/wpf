bd6867b8bf36d51358a8165ddc07eeb8a8a2fa15
stevenmstx <steven@purewavedesigns.com>
2014-01-23 19:59:58 -0600

    Adjusted conditionals and placement of ternaries

--- Assignments/Conditionals/Conditionals_Personal/js/scripts.js
+++ Assignments/Conditionals/Conditionals_Personal/js/scripts.js
@@ -4,17 +4,20 @@ var people = prompt("How many people will be dinning this evening?");
 var eating = prompt("How many of them will be eating rice?");
 var cook = 0;
 var rice = 2;
+var maximum = 20;
 
-if (people && eating == "") {
+if (people == "" && eating == "") {
     console.log("A value is required!");
-}else{
+}else if(eating <= maximum){
     (eating > people) ? console.log("Imaginary friends do not count. Must be less than People dinning.") : cook = 1;
-}
-
-if (cook === 1;){
-    rice = eating / 2 * rice;
-    var water = rice / 2;
-    var result = "You need " + rice + " cups of rice, and " + water + " cups of water."
+    
+    if (cook === 1){
+        rice = eating / 2 * rice;
+        var water = rice / 2;
+        var result = "You need " + rice + " cups of rice, and " + water + " cups of water."
+    }
     console.log(result);
     alert(result);
+}else{
+    (eating > people) ? console.log("Imaginary friends do not count. Must be less than People dinning.") : console.log("Too many mouths to feed!");
 }
