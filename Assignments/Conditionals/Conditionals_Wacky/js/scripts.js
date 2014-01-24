bfd64c87c687ad57b839f515ed996e420b94137f
stevenmstx <steven@purewavedesigns.com>
2014-01-23 21:00:23 -0600

    Comments for variables and main if checker

--- Assignments/Conditionals/Conditionals_Wacky/js/scripts.js
+++ Assignments/Conditionals/Conditionals_Wacky/js/scripts.js
@@ -1,9 +1,9 @@
 // Steven Spencer Jan. 23, 2014 - Conditionals
 // How to find out how much coffee is required to make someone hyper-active, based on age.
-var name = prompt("What is the person's, drinking the coffee, name?");
-if (name == ""){
-    console.log("I need a name!");
-}else{
+var name = prompt("What is the person's, drinking the coffee, name?"); //Prompts user for person's name.
+if (name == ""){ //Checks to see if name is empty.
+    console.log("I need a name!"); //Output in case name is empty.
+}else{ //if name is not empty proceed with calculations.
     var age = prompt("How old is " + name + "?");
     if (age == ""){
         console.log("Enter the age of the person.");
