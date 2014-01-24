2e7a65de5fc48c944930c25135a0aa712481164f
stevenmstx <steven@purewavedesigns.com>
2014-01-23 20:44:38 -0600

    Created Industry variables and operations

--- Assignments/Conditionals/Conditionals_Industry/js/scripts.js
+++ Assignments/Conditionals/Conditionals_Industry/js/scripts.js
@@ -1,1 +1,30 @@
 // Steven Spencer Jan. 23, 2014 - Conditionals
+// This is to calculate how much should be billed to a client for work done during regular hours and overtime hours.
+var client = prompt("Please enter client's name.");
+var rhours = prompt("How many hours did you work during Regular Business time?");
+var ohours = prompt("Hom many hours did you work outside Regular Business time?");
+var rate = 115;
+
+if(client == ""){
+    console.log("Client's name is required!");
+}else if (rhours != "" && ohours != ""){
+    rhours = rhours * rate;
+    ohours = ohours * (rate * 1.5);
+    var result = rhours + ohours;
+    var cost = client + " owes you $" + result;
+    console.log(cost);
+    alert(cost);
+}else if (rhours != "" && ohours == ""){
+    rhours = rhours * rate;
+    var cost = client + " owes you $" + rhours;
+    console.log(cost);
+    alert(cost);
+}else if (rhours == "" && ohours != ""){
+    ohours = ohours * (rate * 1.5);
+    var cost = client + " owes you $" + ohours;
+    console.log(cost);
+    alert(cost);
+}else{
+    console.log("You aren't getting paid!");
+    alert("You aren't getting paid!");
+}
