7d843a0e442118e448d7596f142b8d5d1bad988a
stevenmstx <steven@purewavedesigns.com>
2014-01-23 19:25:15 -0600

    Created variables for personal conditional

--- Assignments/Conditionals/Conditionals_Personal/js/scripts.js
+++ Assignments/Conditionals/Conditionals_Personal/js/scripts.js
@@ -1,1 +1,6 @@
 // Steven Spencer Jan. 23, 2014 - Conditionals
+//How much rice and water is needed to make steamed rice for a certain amount of people. The steamer is limited to a maximum serving of 20 people.
+var people = prompt("How many people will be dinning this evening?");
+var eating = prompt("How many of them will be eating rice?");
+var rice = 2;
+var water = rice / 2;
