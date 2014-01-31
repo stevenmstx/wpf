854955e322060caf2b9a958d28e4f8d4d9be139d
stevenmstx <steven@purewavedesigns.com>
2014-01-30 23:20:27 -0600

    Added comments

--- Assignments/Functions/Functions_Personal/js/scripts.js
+++ Assignments/Functions/Functions_Personal/js/scripts.js
@@ -1,24 +1,25 @@
 // Steven Spencer Jan. 30, 2014 - Functions
 // Calcuate how many eggs I need to make breakfast
 
+// Getting user input
 var people = prompt("How many people are eating breakfast?");
 var scram = prompt("How many of them want scrambled?");
 var over = prompt("How many want over-easy?");
 
-function breakfast(num,scrambled,overeasy) {
-	var p = null;
+function breakfast(num,scrambled,overeasy) { // Declaring a new function with parameters
+	var p = null; // Setting blank variables
 	var s = null;
 	var o = null;
-	(num == "") ? console.log("Please enter a number") : p = num;
+	(num == "") ? console.log("Please enter a number") : p = num; // Terinary Statements
 	(scrambled == "") ? console.log("Please enter a number") : s = scrambled;
 	(overeasy == "") ? console.log("Please enter a number") : o = overeasy;
 	var test = s + o;
 	if (test > p) {
 		console.log("No imaginary friends allowed...");
 	}else{
-		var eggs = parseInt(s,10) * 2 + parseInt(o,10);
+		var eggs = parseInt(s,10) * 2 + parseInt(o,10); //Calculation
 		console.log("You need " + eggs + " eggs to make breakfast.");
 	}
 }
 
-breakfast(people,scram,over);
+breakfast(people,scram,over); // Calling function with arguments
