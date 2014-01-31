c5c2c160cb651542c2b455e17f0d9c830b99a166
stevenmstx <steven@purewavedesigns.com>
2014-01-30 22:47:51 -0600

    Added calculations

--- Assignments/Functions/Functions_Industry/js/scripts.js
+++ Assignments/Functions/Functions_Industry/js/scripts.js
@@ -4,3 +4,20 @@
 var columns = prompt("How many columns do you want to have?");
 var main = prompt("How wide do you want the main content area?");
 
+if (columns == ""){
+	console.log("Enter a value.");
+}else if (main == ""){
+	console.log("Enter a value.");
+}else{
+	function calculate(col,size){
+		if (col >= 4){
+			console.log("Too many columns entered...");
+		}else if (col < 4){
+			var colwidth = parseInt(col,10) * 200;
+			var result = parseInt(colwidth,10) + parseInt(size,10);
+			console.log("You need " + result + " pixels for your web page.");
+		}
+	}
+}
+
+calculate(columns,main);
