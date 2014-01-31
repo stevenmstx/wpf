fe3bdf2da7790db1093e63c87ef85f1b0905d19a
stevenmstx <steven@purewavedesigns.com>
2014-01-30 22:37:39 -0600

    Created variables

--- Assignments/Functions/Functions_Industry/js/scripts.js
+++ Assignments/Functions/Functions_Industry/js/scripts.js
@@ -1,1 +1,6 @@
 // Steven Spencer Jan. 30, 2014 - Functions
+// Calculate the number of pixels needed for a web page.
+
+var columns = prompt("How many columns do you want to have?");
+var main = prompt("How wide do you want the main content area?");
+
